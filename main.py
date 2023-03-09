a34b703efba020f48bad350ea094aba91cdcb207
RioClaroInvestimentos <guilherme.gcml@gmail.com>
2023-03-09 12:27:24 -0300

    feat: adiciona orama

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
 from trello import TrelloClient
 
 from push import geraQuadro
+import pandas as pd
+
 
 token = 'ATTAca1eb3a295f2138fa934ce83b1d173f107829212be830bb23e6d73f113abc06586803B32'
 api_key = '4848089a42753a2e204034c626e7059b'
 board_id ='6408e8c5d254de205a5d7759'
 list_id = '6408e8c5d254de205a5d7761'
 
+
+
+
 # acessar a excel
 # colocar todos os clientes em uma lista com seus respectivos campos
 
 
+
+# aqui criamos o quadro com todos os argumentos...
 titulo = 'TESTE 1000'
 desc = 'Aqui vai dar certo a label'
-label = ['BTG']
-geraQuadro(api_key,token,board_id,list_id,titulo,desc,label)
+corretoras = ['BTG']
+cpf = '07823507129'
+situacao = 'Ativo'
+perfil = 'Moderado'
+planejador = 'Pedro Jobim'
+geraQuadro(api_key,token,board_id,list_id,titulo,desc,corretoras,cpf,situacao,perfil,planejador)
+
+
+
+
--- push.py
+++ push.py
@@ -7,16 +7,17 @@ class Label:
 
 
 # aqui label é uma lista com nomes das corretoras que esse cliente tem.
-def geraQuadro(api_key, token, board_id, list_id, card_title, card_desc, label):
+def geraQuadro(api_key, token, board_id, list_id, card_title, card_desc, corretoras,cpf,situacao,perfil,planejador):
     genial = Label('6408f1543bd760598eb7cd2c', 'Genial')
     btg = Label('6408f15a045603c9f3cf189d', 'BTG')
-    orama = 'a adicionar'
+    orama = Label('6409f97f9201323e745b8e1b', 'Orama')
     interactive = 'a adicionar'
     netx = 'a adicionar'
     
     nomeCorretora = {
-        'genial' : genial,
-        'BTG' : btg 
+        'Genial' : genial,
+        'BTG' : btg,
+        'Órama': orama 
     }
 
     # Criar um cliente Trello
@@ -32,24 +33,23 @@ def geraQuadro(api_key, token, board_id, list_id, card_title, card_desc, label):
     lista_custom_fields = board.get_custom_field_definitions()
     
     labelAdicionar = []
-    if len(label) > 1:
+    if len(corretoras) > 1:
         print('adicionar mais de uma corretora a lista')
     else:
-        labelAdicionar.append(nomeCorretora[label[0]])
+        labelAdicionar.append(nomeCorretora[corretoras[0]])
     
     # adicionar o cartão com os campos personalizados(criar função) LABEL é uma lista com as classes label
     card = list.add_card(name=card_title, desc=card_desc, labels=labelAdicionar)
-    
     
     # adicionando o costum field ao cartão...
     for i in range(4):
         campo_em_questao = lista_custom_fields[i] #escrita no cardeno
         if campo_em_questao.name == "Situação":
-            valor_custom_field = 'Ativo'
+            valor_custom_field = situacao
         elif campo_em_questao.name == "Planejador":
-            valor_custom_field = 'Leonardo Pavan'
+            valor_custom_field = planejador
         elif campo_em_questao.name == "Perfil":
-            valor_custom_field = 'Moderado'
+            valor_custom_field = perfil
         else:
-            valor_custom_field = '07823507129'
+            valor_custom_field = cpf
         card.set_custom_field(valor_custom_field, campo_em_questao)
